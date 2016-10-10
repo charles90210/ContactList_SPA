d5e979f245ba04c03560b0e22d0b6271b97ba427
Charles Ukpai <charles.ukpai@yahoo.com>
2016-10-10 13:22:17 -0700

    first commit angular contact app

---
+++ assets/contacts.js
@@ -0,0 +1,16 @@
+(function(){
+
+var app = angular.module('myContacts', []);
+//Main contacts controller
+app.controller('ContactsController', ['$scope', function(){
+
+alert('hello world');
+
+
+
+}]);
+
+
+
+
+})();//end of main function
