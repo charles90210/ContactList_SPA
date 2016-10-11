950f04147c6194f60a1bc4c53e2f00c7f06f046d
Charles Ukpai <charles.ukpai@yahoo.com>
2016-10-11 14:04:13 -0700

    got json data to display in bootstrap table

--- assets/contacts.js
+++ assets/contacts.js
@@ -1,16 +1,18 @@
 (function(){
-
+//Initializing our app module
 var app = angular.module('myContacts', []);
+
 //Main contacts controller
-app.controller('ContactsController', ['$scope', function(){
-
-alert('hello world');
+app.controller('ContactsController', ['$scope', '$http', function($scope, $http){
 
 
+//$http get service to help us retrieve the json data
+$http.get('assets/contacts.json').success(function(data){
+    $scope.contacts = data;
+});
+  //This function here allows me display my json data in my bootstrap table.  
+  $(function(){$('#table').bootstrapTable({data: contacts})})
 
 }]);
-
-
-
 
 })();//end of main function
