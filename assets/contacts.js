8bd5066834db19dc9f69c7885d83467b6c640d75
Charles Ukpai <charles.ukpai@yahoo.com>
2016-10-12 13:07:31 -0700

    added new contact form working

--- assets/contacts.js
+++ assets/contacts.js
@@ -1,10 +1,31 @@
 (function(){
-//Initializing our app module
+//Initializing our app module, added the dependency for pagination
 var app = angular.module('myContacts', ['angularUtils.directives.dirPagination']);
 
 //Main contacts controller
 app.controller('ContactsController', ['$scope', '$http', function($scope, $http){
 
+//Function allowing us to add new contacts to the database
+$scope.addContact = function(){
+$scope.contacts.push({
+  id: $scope.newcontact.id
+  Name: $scope.newcontact.Name,
+  Email: $scope.newcontact.Email,
+  Phone: $scope.newcontact.Phone,
+  Address: $scope.newcontact.Address,
+  City: $scope.newcontact.City,
+  Zipcode: $scope.newcontact.Zipcode,
+  State: $scope.newcontact.State
+});
+//Setting up our form so it clears out after each new contact is added
+  $scope.newcontact.Name = "";
+  $scope.newcontact.Email = "";
+  $scope.newcontact.Phone = "";
+  $scope.newcontact.Address = "";
+  $scope.newcontact.City = "";
+  $scope.newcontact.Zipcode = "";
+  $scope.newcontact.State = "";
+};
 
 $scope.contacts = [];
 //$http get service to help us retrieve the json data
