1ca993a461a89b97a43654e2837cffcbe5192c95
Charles Ukpai <charles.ukpai@yahoo.com>
2016-10-12 06:46:36 -0700

    Added pagination and sort

--- assets/contacts.js
+++ assets/contacts.js
@@ -1,17 +1,25 @@
 (function(){
 //Initializing our app module
-var app = angular.module('myContacts', []);
+var app = angular.module('myContacts', ['angularUtils.directives.dirPagination']);
 
 //Main contacts controller
 app.controller('ContactsController', ['$scope', '$http', function($scope, $http){
 
 
+$scope.contacts = [];
 //$http get service to help us retrieve the json data
 $http.get('assets/contacts.json').success(function(data){
     $scope.contacts = data;
+//This function here allows me push my json data into the bootstrap Table 
+$(function(){$('#table').bootstrapTable({data: contacts})})
+
 });
-  //This function here allows me display my json data in my bootstrap table.  
-  $(function(){$('#table').bootstrapTable({data: contacts})})
+   
+//Setting up sort for the table columns  
+$scope.sort = function(keyname){
+  $scope.sortKey = keyname;
+  $scope.reverse = !$scope.reverse;
+}
 
 }]);
 
